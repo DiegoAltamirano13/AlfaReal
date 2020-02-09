d7c9321b6cc74e36c8d63050077ffe71843bace3
mauriciolp <marc_k86@hotmail.com>
2020-02-09 13:01:39 -0600

    Lista expandible

--- app/src/main/java/com/diego/lina/sistemadealmacenes/principal_pagina_menu.java
+++ app/src/main/java/com/diego/lina/sistemadealmacenes/principal_pagina_menu.java
@@ -35,19 +35,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.diego.lina.sistemadealmacenes.Adaptador.ExpandListAdapter;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class principal_pagina_menu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     TextView nombre;
+    List<String> listDataHeader;
+    HashMap<String, List<String>>  listDataChild;
+    ExpandableListView expListView;
+
     //Pruebas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +73,8 @@ public class principal_pagina_menu extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        /*Invocar listview expandible*/
+        enableExpandableList();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -148,5 +161,104 @@ public class principal_pagina_menu extends AppCompatActivity
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    /*Lista expandible*/
+    private void enableExpandableList() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+        expListView = (ExpandableListView) findViewById(R.id.left_drawer);
+
+        prepareListData(listDataHeader, listDataChild);
+        ExpandListAdapter listAdapter = new ExpandListAdapter(this, listDataHeader, listDataChild);
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                // Temporary code:
+
+                // till here
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });}
+
+
+    private void prepareListData(List<String> listDataHeader, Map<String,
+            List<String>> listDataChild) {
+
+
+        // Adding child data
+        listDataHeader.add("Inventarios");
+        listDataHeader.add("Comercio Exterior");
+        listDataHeader.add("Certificaciòn");
+
+        // Adding child data
+        List<String> top = new ArrayList<String>();
+        top.add("Saldo UME por UMC");
+        top.add("Saldo desglosado");
+        top.add("Saldo desglosado");
+        top.add("Saldo desglosado(Lote)");
+
+
+        List<String> mid = new ArrayList<String>();
+        mid.add("Saldo pagado");
+
+
+        List<String> bottom = new ArrayList<String>();
+        bottom.add("Saldo (certificado)");
+
+
+
+        listDataChild.put(listDataHeader.get(0), top); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), mid);
+        listDataChild.put(listDataHeader.get(2), bottom);
     }
 }
